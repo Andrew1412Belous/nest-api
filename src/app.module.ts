92067629b56251c3bc2a3d456663d1986f2dce60
Andrii <andrew.belous1412@gmail.com>
2023-06-23 11:20:16 +0300

    add mongo config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,19 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot('mongodb://localhost/test'),
 		ConfigModule.forRoot(),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig,
+		}),
 		AuthModule,
 		TopPageModule,
 		ProductModule,
