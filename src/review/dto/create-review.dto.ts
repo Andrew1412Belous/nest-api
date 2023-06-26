b5e31fd800759dfcd7bd8b217b95104a6ae4b8a7
Andrii <andrew.belous1412@gmail.com>
2023-06-26 13:44:22 +0300

    add review validation

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,20 @@
+import { IsString, IsNumber, Max, Min } from 'class-validator';
+
 export class CreateReviewDto {
+	@IsString()
 	name: string;
+
+	@IsString()
 	title: string;
+
+	@IsString()
 	description: string;
+
+	@Max(5, { message: "Rating can't be higher than 1" })
+	@Min(1, { message: "Rating can't be lower than 1" })
+	@IsNumber()
 	rating: number;
+
+	@IsString()
 	productId: string;
 }
